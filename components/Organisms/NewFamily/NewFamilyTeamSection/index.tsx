bf1870a21217fe53decfb08ca87792ad98729e68
Covenant Nomad <covenantnomad@Covenantui-Macmini.local>
2023-12-03 21:50:31 +0900

    fix: edit newFamily UI, add viceReader to attendanceList

--- components/Organisms/NewFamily/NewFamilyTeamSection/index.tsx
+++ components/Organisms/NewFamily/NewFamilyTeamSection/index.tsx
@@ -10,7 +10,7 @@ const NewFamilyTeamSection = ({ teamList }: NewFamilyTeamSectionProps) => {
   return (
     <div>
       <h6 className="text-xl font-bold">새가족부 멤버</h6>
-      <div className="w-full mx-auto grid grid-cols-2 gap-y-12 gap-x-6 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6">
+      <div className="w-full mx-auto grid grid-cols-2 gap-y-12 gap-x-6 py-4 lg:py-6 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6">
         {teamList.map(member => (
           <NewFamilyListItem key={member.id} member={member} />
         ))}
