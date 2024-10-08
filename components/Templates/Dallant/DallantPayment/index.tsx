384aede432d850653caa1f4493dfd39a3526de1d
Covenant Nomad <covenantnomad@Covenantui-Macmini.local>
2024-10-08 16:00:03 +0900

    fix: 달란트잔치, 이예찬셀 포함

--- components/Templates/Dallant/DallantPayment/index.tsx
+++ components/Templates/Dallant/DallantPayment/index.tsx
@@ -1,159 +1,174 @@
-import React, { useEffect, useState } from 'react';
+import {useEffect, useState} from "react";
 //data
-import graphlqlRequestClient from '../../../../client/graphqlRequestClient';
-import { FIND_CELL_LIMIT } from '../../../../constants/constant';
-import { FindCellsWithMembersQuery, FindCellsWithMembersQueryVariables, useFindCellsWithMembersQuery } from '../../../../graphql/generated';
+import graphlqlRequestClient from "../../../../client/graphqlRequestClient";
+import {FIND_CELL_LIMIT} from "../../../../constants/constant";
+import {
+  FindCellsWithMembersQuery,
+  FindCellsWithMembersQueryVariables,
+  useFindCellsWithMembersQuery,
+} from "../../../../graphql/generated";
 //types
-import { DallantsSettingType } from '../../../../interface/Dallants';
-import { CellListWithMemberType, SpecialCellIdType } from '../../../../interface/cell';
+import {
+  CellListWithMemberType,
+  SpecialCellIdType,
+} from "../../../../interface/cell";
 //utils
-import { onHandleCopy } from '../../../../utils/utils';
+import {onHandleCopy} from "../../../../utils/utils";
 //components
-import BlockContainer from '../../../Atoms/Container/BlockContainer';
-import Spinner from '../../../Atoms/Spinner';
-import DallantPaymentCommunity from '../DallantPaymentCommunity';
-import UnderlineTabs from '../../../Atoms/Tabs/UnderlineTabs';
-import DallantPaymetOthers from '../DallantPaymetOthers';
-import SimpleAlerts from '../../../Atoms/Alerts/SimpleAlerts';
-
-
+import SimpleAlerts from "../../../Atoms/Alerts/SimpleAlerts";
+import BlockContainer from "../../../Atoms/Container/BlockContainer";
+import Spinner from "../../../Atoms/Spinner";
+import UnderlineTabs from "../../../Atoms/Tabs/UnderlineTabs";
+import DallantPaymentCommunity from "../DallantPaymentCommunity";
+import DallantPaymetOthers from "../DallantPaymetOthers";
 
 const tabs = [
   {
     id: 0,
-    name: "길"
+    name: "길",
   },
   {
     id: 1,
-    name: "진리"
+    name: "진리",
   },
   {
     id: 2,
-    name: "생명"
+    name: "생명",
   },
   {
     id: 3,
-    name: "빛"
+    name: "빛",
   },
   {
     id: 4,
-    name: "셀 미포함"
-  }
-]
+    name: "셀 미포함",
+  },
+];
 
 const recommendText = [
   {
     id: 0,
-    text: '선포빙고 챌린지'
+    text: "선포빙고 챌린지",
   },
   {
     id: 1,
-    text: '캐스트 기도운동'
-  }
-]
+    text: "캐스트 기도운동",
+  },
+];
 
 interface DallantPaymentProps {}
 
 const DallantPayment = ({}: DallantPaymentProps) => {
-  const [ currentTab, setCurrentTab ] = useState(tabs[0].name)
-  const [ lightCommunity, setLightCommunity ] = useState<CellListWithMemberType[]>([]);
-  const [ wayCommunity, setWayCommunity ] = useState<CellListWithMemberType[]>([]);
-  const [ lifeCommunity, setLifeCommunity ] = useState<CellListWithMemberType[]>([]);
-  const [ truthCommunity, setTruthCommunity ] = useState<CellListWithMemberType[]>([]);
-  const [ otherCells, setOtherCells ] = useState<CellListWithMemberType[]>([]);
-  const { isLoading: isCellLoading, data: cellData } = useFindCellsWithMembersQuery<
-    FindCellsWithMembersQuery,
-    FindCellsWithMembersQueryVariables
-  >(
-    graphlqlRequestClient,
-    {
-      limit: FIND_CELL_LIMIT,
-    },
-    {
-      staleTime: 60 * 60 * 1000,
-      cacheTime: 60 * 60 * 1000 * 24,
-    }
+  const [currentTab, setCurrentTab] = useState(tabs[0].name);
+  const [lightCommunity, setLightCommunity] = useState<
+    CellListWithMemberType[]
+  >([]);
+  const [wayCommunity, setWayCommunity] = useState<CellListWithMemberType[]>(
+    []
   );
+  const [lifeCommunity, setLifeCommunity] = useState<CellListWithMemberType[]>(
+    []
+  );
+  const [truthCommunity, setTruthCommunity] = useState<
+    CellListWithMemberType[]
+  >([]);
+  const [otherCells, setOtherCells] = useState<CellListWithMemberType[]>([]);
+  const {isLoading: isCellLoading, data: cellData} =
+    useFindCellsWithMembersQuery<
+      FindCellsWithMembersQuery,
+      FindCellsWithMembersQueryVariables
+    >(
+      graphlqlRequestClient,
+      {
+        limit: FIND_CELL_LIMIT,
+      },
+      {
+        staleTime: 60 * 60 * 1000,
+        cacheTime: 60 * 60 * 1000 * 24,
+      }
+    );
 
   useEffect(() => {
     if (cellData) {
-      setLightCommunity(cellData.findCells.nodes
-        .filter(
-          (cell) =>
-            !cell.id.includes(SpecialCellIdType.NewFamily) &&
-            !cell.id.includes(SpecialCellIdType.Blessing) &&
-            !cell.id.includes(SpecialCellIdType.Renew)
-        )
-        .filter(
-          (cell) => cell.community === "빛"
-        )
-        .sort((a, b) => {
-          if (a.name > b.name) return 1;
-          else if (b.name > a.name) return -1;
-          else return 0;
-        })
-      ) 
-      setWayCommunity(cellData.findCells.nodes
-        .filter(
-          (cell) =>
-            !cell.id.includes(SpecialCellIdType.NewFamily) &&
-            !cell.id.includes(SpecialCellIdType.Blessing) &&
-            !cell.id.includes(SpecialCellIdType.Renew)
-        )
-        .filter(
-          (cell) => cell.community === "길"
-        )
-        .sort((a, b) => {
-          if (a.name > b.name) return 1;
-          else if (b.name > a.name) return -1;
-          else return 0;
-        })
-      )
-      setLifeCommunity(cellData.findCells.nodes
-        .filter(
-          (cell) =>
-            !cell.id.includes(SpecialCellIdType.NewFamily) &&
-            !cell.id.includes(SpecialCellIdType.Blessing) &&
-            !cell.id.includes(SpecialCellIdType.Renew)
-        )
-        .filter(
-          (cell) => cell.community === "생명"
-        )
-        .sort((a, b) => {
-          if (a.name > b.name) return 1;
-          else if (b.name > a.name) return -1;
-          else return 0;
-        })
-      )
-      setTruthCommunity(cellData.findCells.nodes
-        .filter(
-          (cell) =>
-            !cell.id.includes(SpecialCellIdType.NewFamily) &&
-            !cell.id.includes(SpecialCellIdType.Blessing) &&
-            !cell.id.includes(SpecialCellIdType.Renew)
-        )
-        .filter(
-          (cell) => cell.community === "진리"
-        )
-        .sort((a, b) => {
-          if (a.name > b.name) return 1;
-          else if (b.name > a.name) return -1;
-          else return 0;
-        })
-      )
-      setOtherCells(cellData.findCells.nodes
-        .filter(
-          (cell) =>
-            cell.id.includes(SpecialCellIdType.NewFamily) ||
-            cell.id.includes(SpecialCellIdType.Blessing) ||
-            cell.id.includes(SpecialCellIdType.Renew)
-        )
-        .sort((a, b) => {
-          if (a.name > b.name) return 1;
-          else if (b.name > a.name) return -1;
-          else return 0;
-        })
-      )
+      setLightCommunity(
+        cellData.findCells.nodes
+          .filter(
+            (cell) =>
+              !cell.id.includes(SpecialCellIdType.NewFamily) &&
+              !cell.id.includes(SpecialCellIdType.NewFamilyTwo) &&
+              !cell.id.includes(SpecialCellIdType.Blessing) &&
+              !cell.id.includes(SpecialCellIdType.Renew)
+          )
+          .filter((cell) => cell.community === "빛")
+          .sort((a, b) => {
+            if (a.name > b.name) return 1;
+            else if (b.name > a.name) return -1;
+            else return 0;
+          })
+      );
+      setWayCommunity(
+        cellData.findCells.nodes
+          .filter(
+            (cell) =>
+              !cell.id.includes(SpecialCellIdType.NewFamily) &&
+              !cell.id.includes(SpecialCellIdType.NewFamilyTwo) &&
+              !cell.id.includes(SpecialCellIdType.Blessing) &&
+              !cell.id.includes(SpecialCellIdType.Renew)
+          )
+          .filter((cell) => cell.community === "길")
+          .sort((a, b) => {
+            if (a.name > b.name) return 1;
+            else if (b.name > a.name) return -1;
+            else return 0;
+          })
+      );
+      setLifeCommunity(
+        cellData.findCells.nodes
+          .filter(
+            (cell) =>
+              !cell.id.includes(SpecialCellIdType.NewFamily) &&
+              !cell.id.includes(SpecialCellIdType.NewFamilyTwo) &&
+              !cell.id.includes(SpecialCellIdType.Blessing) &&
+              !cell.id.includes(SpecialCellIdType.Renew)
+          )
+          .filter((cell) => cell.community === "생명")
+          .sort((a, b) => {
+            if (a.name > b.name) return 1;
+            else if (b.name > a.name) return -1;
+            else return 0;
+          })
+      );
+      setTruthCommunity(
+        cellData.findCells.nodes
+          .filter(
+            (cell) =>
+              !cell.id.includes(SpecialCellIdType.NewFamily) &&
+              !cell.id.includes(SpecialCellIdType.NewFamilyTwo) &&
+              !cell.id.includes(SpecialCellIdType.Blessing) &&
+              !cell.id.includes(SpecialCellIdType.Renew)
+          )
+          .filter((cell) => cell.community === "진리")
+          .sort((a, b) => {
+            if (a.name > b.name) return 1;
+            else if (b.name > a.name) return -1;
+            else return 0;
+          })
+      );
+      setOtherCells(
+        cellData.findCells.nodes
+          .filter(
+            (cell) =>
+              cell.id.includes(SpecialCellIdType.NewFamily) ||
+              cell.id.includes(SpecialCellIdType.NewFamilyTwo) ||
+              cell.id.includes(SpecialCellIdType.Blessing) ||
+              cell.id.includes(SpecialCellIdType.Renew)
+          )
+          .sort((a, b) => {
+            if (a.name > b.name) return 1;
+            else if (b.name > a.name) return -1;
+            else return 0;
+          })
+      );
     }
   }, [cellData]);
 
@@ -161,38 +176,64 @@ const DallantPayment = ({}: DallantPaymentProps) => {
     <>
       <BlockContainer firstBlock>
         <div>
-          <div className=''>
-            <SimpleAlerts 
-              title={"입력시 주의사항"} 
-              description={`1. 금액을 입력하면 꼭 내역을 입력해주세요.\n2. 금액은 1이상의 양수만 입력해주세요.\n3. 엔터를 누르면 바로 제출됩니다. 탭으로 이동해주세요.\n4. 한번 제출하면 다른 공동체로 갔다와주세요. 데이터가 남아 있어요ㅠ`} 
+          <div className="">
+            <SimpleAlerts
+              title={"입력시 주의사항"}
+              description={`1. 금액을 입력하면 꼭 내역을 입력해주세요.\n2. 금액은 1이상의 양수만 입력해주세요.\n3. 엔터를 누르면 바로 제출됩니다. 탭으로 이동해주세요.\n4. 한번 제출하면 다른 공동체로 갔다와주세요. 데이터가 남아 있어요ㅠ`}
             />
-            <div className='h-2'/>
-            <UnderlineTabs tabs={tabs} currentTab={currentTab} setCurrentTab={setCurrentTab} />
+            <div className="h-2" />
+            <UnderlineTabs
+              tabs={tabs}
+              currentTab={currentTab}
+              setCurrentTab={setCurrentTab}
+            />
           </div>
-          <div className='flex justify-center items-center py-8'>
-            <p className='mr-8 text-base text-gray-700'>추천 내역</p>
-            <div className='flex gap-x-3'>
-              {recommendText.map(item => (
-                <button 
+          <div className="flex justify-center items-center py-8">
+            <p className="mr-8 text-base text-gray-700">추천 내역</p>
+            <div className="flex gap-x-3">
+              {recommendText.map((item) => (
+                <button
                   key={item.id}
-                  onClick={() => onHandleCopy(item.text)} 
-                  className='px-3 py-1 rounded-2xl border bg-gray-100 text-sm text-gray-700'
+                  onClick={() => onHandleCopy(item.text)}
+                  className="px-3 py-1 rounded-2xl border bg-gray-100 text-sm text-gray-700"
                 >
                   {item.text}
                 </button>
               ))}
             </div>
           </div>
-          <div className='mt-8'>
+          <div className="mt-8">
             {isCellLoading ? (
               <Spinner />
             ) : (
               <div>
-                {currentTab === "길" && <DallantPaymentCommunity cells={wayCommunity} communityName={"길"} />}
-                {currentTab === "진리" && <DallantPaymentCommunity cells={truthCommunity} communityName={"진리"}/>}
-                {currentTab === "생명" && <DallantPaymentCommunity cells={lifeCommunity} communityName={"생명"}/>}
-                {currentTab === "빛" && <DallantPaymentCommunity cells={lightCommunity} communityName={"빛"}/>}
-                {currentTab === "셀 미포함" && <DallantPaymetOthers cells={otherCells} />}
+                {currentTab === "길" && (
+                  <DallantPaymentCommunity
+                    cells={wayCommunity}
+                    communityName={"길"}
+                  />
+                )}
+                {currentTab === "진리" && (
+                  <DallantPaymentCommunity
+                    cells={truthCommunity}
+                    communityName={"진리"}
+                  />
+                )}
+                {currentTab === "생명" && (
+                  <DallantPaymentCommunity
+                    cells={lifeCommunity}
+                    communityName={"생명"}
+                  />
+                )}
+                {currentTab === "빛" && (
+                  <DallantPaymentCommunity
+                    cells={lightCommunity}
+                    communityName={"빛"}
+                  />
+                )}
+                {currentTab === "셀 미포함" && (
+                  <DallantPaymetOthers cells={otherCells} />
+                )}
               </div>
             )}
           </div>
--- hooks/useCheckCellAttendanceSubmissions.ts
+++ hooks/useCheckCellAttendanceSubmissions.ts
@@ -1,106 +1,134 @@
-import { useEffect, useState } from "react"
-import { CellLeaderAttendanceSubmissionStatus, CheckCellAttendanceSubmissionsQuery, CheckCellAttendanceSubmissionsQueryVariables, useCheckCellAttendanceSubmissionsQuery } from "../graphql/generated";
+import {useEffect, useState} from "react";
 import graphlqlRequestClient from "../client/graphqlRequestClient";
+import {
+  CellLeaderAttendanceSubmissionStatus,
+  CheckCellAttendanceSubmissionsQuery,
+  CheckCellAttendanceSubmissionsQueryVariables,
+  useCheckCellAttendanceSubmissionsQuery,
+} from "../graphql/generated";
 //types
-import { SpecialCellIdType } from "../interface/cell";
-import { AttendanceSubmissionType } from "../interface/attendance";
-import { CommunityFilter } from "../stores/cellState";
+import {AttendanceSubmissionType} from "../interface/attendance";
+import {SpecialCellIdType} from "../interface/cell";
+import {CommunityFilter} from "../stores/cellState";
 
-const useCheckCellAttendanceSubmissions = ( attendanceDate: string ) => {
-  const [ isLoading, setIsLoading ] = useState<boolean>(false)
-  const [ attendanceStatus, setAttendanceStatus ] = useState<boolean>(false)
-  const [ communityWay, setCommunityWay ] = useState<AttendanceSubmissionType[] | null>(null)
-  const [ communityLife, setCommunityLife ] = useState<AttendanceSubmissionType[] | null>(null)
-  const [ communityTruth, setCommunityTruth ] = useState<AttendanceSubmissionType[] | null>(null)
-  const [ communityLight, setCommunityLight ] = useState<AttendanceSubmissionType[] | null>(null)
-  const [ specialCells, setSpecialCells ] = useState<AttendanceSubmissionType[] | null>(null)
-  const { isLoading: isDataLoading, isFetching, data } = useCheckCellAttendanceSubmissionsQuery<
-    CheckCellAttendanceSubmissionsQuery, 
+const useCheckCellAttendanceSubmissions = (attendanceDate: string) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [attendanceStatus, setAttendanceStatus] = useState<boolean>(false);
+  const [communityWay, setCommunityWay] = useState<
+    AttendanceSubmissionType[] | null
+  >(null);
+  const [communityLife, setCommunityLife] = useState<
+    AttendanceSubmissionType[] | null
+  >(null);
+  const [communityTruth, setCommunityTruth] = useState<
+    AttendanceSubmissionType[] | null
+  >(null);
+  const [communityLight, setCommunityLight] = useState<
+    AttendanceSubmissionType[] | null
+  >(null);
+  const [specialCells, setSpecialCells] = useState<
+    AttendanceSubmissionType[] | null
+  >(null);
+  const {
+    isLoading: isDataLoading,
+    isFetching,
+    data,
+  } = useCheckCellAttendanceSubmissionsQuery<
+    CheckCellAttendanceSubmissionsQuery,
     CheckCellAttendanceSubmissionsQueryVariables
   >(
     graphlqlRequestClient,
     {
-      attendanceDate: attendanceDate
+      attendanceDate: attendanceDate,
     },
     {
       staleTime: 15 * 60 * 1000,
       cacheTime: 30 * 60 * 1000,
-      enabled: Boolean(attendanceDate)
+      enabled: Boolean(attendanceDate),
     }
-  )
+  );
 
   useEffect(() => {
     if (!isDataLoading && !isFetching) {
-
       if (data && data.cellAttendanceCheckSubmissions) {
-  
-        const commonCell = data.cellAttendanceCheckSubmissions.filter((cell) =>
-          !cell.cellId.includes(SpecialCellIdType.NewFamily) &&
-          !cell.cellId.includes(SpecialCellIdType.Blessing) &&
-          !cell.cellId.includes(SpecialCellIdType.Renew)
-        )
+        const commonCell = data.cellAttendanceCheckSubmissions.filter(
+          (cell) =>
+            !cell.cellId.includes(SpecialCellIdType.NewFamily) &&
+            !cell.cellId.includes(SpecialCellIdType.NewFamilyTwo) &&
+            !cell.cellId.includes(SpecialCellIdType.Blessing) &&
+            !cell.cellId.includes(SpecialCellIdType.Renew)
+        );
 
-        setSpecialCells(data.cellAttendanceCheckSubmissions.filter((cell) =>
-          cell.cellId.includes(SpecialCellIdType.NewFamily) ||
-          cell.cellId.includes(SpecialCellIdType.Blessing) ||
-          cell.cellId.includes(SpecialCellIdType.Renew)
-        ))
+        setSpecialCells(
+          data.cellAttendanceCheckSubmissions.filter(
+            (cell) =>
+              cell.cellId.includes(SpecialCellIdType.NewFamily) ||
+              cell.cellId.includes(SpecialCellIdType.NewFamilyTwo) ||
+              cell.cellId.includes(SpecialCellIdType.Blessing) ||
+              cell.cellId.includes(SpecialCellIdType.Renew)
+          )
+        );
 
-        setCommunityWay(commonCell
-          .filter(item => item.cellCommunity === CommunityFilter.WAY)
-          .sort((a, b) => {
-            if (a.cellName > b.cellName) return 1;
-            else if (b.cellName > a.cellName) return -1;
-            else return 0;
-          })
-        )
-        setCommunityLife(commonCell
-          .filter(item => item.cellCommunity === CommunityFilter.LIFE)
-          .sort((a, b) => {
-            if (a.cellName > b.cellName) return 1;
-            else if (b.cellName > a.cellName) return -1;
-            else return 0;
-          })
-        )
-        setCommunityTruth(commonCell
-          .filter(item => item.cellCommunity === CommunityFilter.TRUTH)
-          .sort((a, b) => {
-            if (a.cellName > b.cellName) return 1;
-            else if (b.cellName > a.cellName) return -1;
-            else return 0;
-          })
-        )
-        setCommunityLight(commonCell
-          .filter(item => item.cellCommunity === CommunityFilter.LIGHT)
-          .sort((a, b) => {
-            if (a.cellName > b.cellName) return 1;
-            else if (b.cellName > a.cellName) return -1;
-            else return 0;
-          })
-        )
+        setCommunityWay(
+          commonCell
+            .filter((item) => item.cellCommunity === CommunityFilter.WAY)
+            .sort((a, b) => {
+              if (a.cellName > b.cellName) return 1;
+              else if (b.cellName > a.cellName) return -1;
+              else return 0;
+            })
+        );
+        setCommunityLife(
+          commonCell
+            .filter((item) => item.cellCommunity === CommunityFilter.LIFE)
+            .sort((a, b) => {
+              if (a.cellName > b.cellName) return 1;
+              else if (b.cellName > a.cellName) return -1;
+              else return 0;
+            })
+        );
+        setCommunityTruth(
+          commonCell
+            .filter((item) => item.cellCommunity === CommunityFilter.TRUTH)
+            .sort((a, b) => {
+              if (a.cellName > b.cellName) return 1;
+              else if (b.cellName > a.cellName) return -1;
+              else return 0;
+            })
+        );
+        setCommunityLight(
+          commonCell
+            .filter((item) => item.cellCommunity === CommunityFilter.LIGHT)
+            .sort((a, b) => {
+              if (a.cellName > b.cellName) return 1;
+              else if (b.cellName > a.cellName) return -1;
+              else return 0;
+            })
+        );
 
-        if (commonCell.filter(cell => cell.submissionStatus !== CellLeaderAttendanceSubmissionStatus.Complete).length === 0) {
-          setAttendanceStatus(true)
+        if (
+          commonCell.filter(
+            (cell) =>
+              cell.submissionStatus !==
+              CellLeaderAttendanceSubmissionStatus.Complete
+          ).length === 0
+        ) {
+          setAttendanceStatus(true);
         } else {
-          setAttendanceStatus(false)
+          setAttendanceStatus(false);
         }
-
       } else {
-        setCommunityWay(null)
-        setCommunityLife(null)
-        setCommunityTruth(null)
-        setCommunityLight(null)
+        setCommunityWay(null);
+        setCommunityLife(null);
+        setCommunityTruth(null);
+        setCommunityLight(null);
       }
 
-      setIsLoading(false)
-
+      setIsLoading(false);
     } else {
-      setIsLoading(true)
+      setIsLoading(true);
     }
-
-
-  }, [isDataLoading, isFetching, data])
-
+  }, [isDataLoading, isFetching, data]);
 
   return {
     isLoading,
@@ -109,8 +137,8 @@ const useCheckCellAttendanceSubmissions = ( attendanceDate: string ) => {
     communityLife,
     communityTruth,
     communityLight,
-    specialCells
-  }
-}
+    specialCells,
+  };
+};
 
 export default useCheckCellAttendanceSubmissions;
