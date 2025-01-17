c57f49bad2c7496f0020b90afe3e0d229e768fba
Covenant Nomad <covenantnomad@Covenantui-Macmini.local>
2025-01-17 20:57:21 +0900

    fix:셀모임 통계기능 최종수정

--- components/Templates/Attendance/CellMeetingStatistic/_components/CellMeetingStaticHistorical/_components/CellMeetingHistoricalWeekly/_components/CellMeetingHistoricalWeeklyTable/index.tsx
+++ components/Templates/Attendance/CellMeetingStatistic/_components/CellMeetingStaticHistorical/_components/CellMeetingHistoricalWeekly/_components/CellMeetingHistoricalWeeklyTable/index.tsx
@@ -18,7 +18,7 @@ const CellMeetingHistoricalWeeklyTable = ({data}: Props) => {
     <Table className="table-fixed">
       <TableHead>
         <TableRow>
-          <TableHeaderCell className="text-center w-32">월간</TableHeaderCell>
+          <TableHeaderCell className="text-center w-32">주간</TableHeaderCell>
           <TableHeaderCell className="text-center">총원</TableHeaderCell>
           <TableHeaderCell className="text-center">참석</TableHeaderCell>
           <TableHeaderCell className="text-center">미참석</TableHeaderCell>
