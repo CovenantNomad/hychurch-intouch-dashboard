55d47a3f5acd25b031d3e9e9d1e2ae7d698d20c6
Covenant Nomad <covenantnomad@Covenantui-Macmini.local>
2026-01-10 21:22:02 +0900

    fix: 2026 표어 교체

--- components/Atoms/Slogan/Slogan.tsx
+++ components/Atoms/Slogan/Slogan.tsx
@@ -4,7 +4,7 @@ const Slogan = ({}: SloganProps) => {
   return (
     <div className="flex text-right mt-3">
       <p className="whitespace-pre-line text-2xl font-nanumBrush font-semibold text-cyan-700 tracking-wider">
-        {`내 삶의 주인 예수 그리스도!`}
+        {`일어나 빛을 발하라`}
       </p>
     </div>
   );
